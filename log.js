d0a0a1b52319005dc20f661641f3cdeeda0a1af7
mastergroup <juli_tepa@hotmail.com>
2024-12-31 03:32:08 -0300

    cambios3

--- log.js
+++ log.js
@@ -8,7 +8,7 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
 
     // Genera el enlace de WhatsApp
     const phoneNumber = "1138479551"; // Cambia por tu número de WhatsApp con código internacional
-    const whatsappURL = `https://api.whatsapp.com/send?phone=${1138479551}&text=Hola,%20soy%20${name}.%20Mi%20correo%20es%20${email}.%20Mensaje:%20${encodeURIComponent(message)}`;
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phone}&text=Hola,%20soy%20${name}.%20Mi%20correo%20es%20${email}.%20Mensaje:%20${encodeURIComponent(message)}`;
 
     // Redirige al usuario a WhatsApp
     window.open(whatsappURL, "_blank");
